2e9f2ececec9b830d0b40e900873c3d83299216a
gongyulin <2943765828@qq.com>
2022-04-19 22:19:47 +0800

    third commit

--- 16_src_TodoList_nextTick/components/MyItem.vue
+++ 16_src_TodoList_nextTick/components/MyItem.vue
@@ -2,13 +2,22 @@
   <li>
     <label>
       <input type="checkbox" :checked="todo.done" @change="handleCheck(todo.id)">
-      <span>{{ todo.title }}</span>
+      <span v-show="!todo.isEdit">{{ todo.title }}</span>
+      <input
+        type="text"
+        v-show="todo.isEdit"
+        :value="todo.title"
+        @blur="handlerBlur(todo, $event)"
+        ref="inputTitle"
+      />
     </label>
     <button class="btn btn-danger" @click="handleDelete(todo.id)">删除</button>
+    <button v-show="!todo.isEdit" class="btn btn-edit" @click="handleEdit(todo)">编辑</button>
   </li>
 </template>
 
 <script>
+import pubsub from 'pubsub-js'
   export default {
     name:'MyItem',
     // 声明接收todo对象
@@ -24,8 +33,27 @@
       handleDelete(id){
         if(confirm('确定删除吗？')){
           // this.deleteTodo(id)
-          this.$bus.$emit('deleteTodo', id)
+          // this.$bus.$emit('deleteTodo', id)
+          pubsub.publish('deleteTodo', id)
         }
+      },
+      // 编辑
+      handleEdit(todo){
+        if(todo.hasOwnProperty('isEdit')){
+          todo.isEdit = true
+        } else {
+          // console.log('@');
+          this.$set(todo, 'isEdit', true)
+        }
+        this.$nextTick(function(){
+          this.$refs.inputTitle.focus()
+        })
+      },
+      // 失去焦点回调（真正执行修改逻辑）
+      handlerBlur(todo, e){
+        todo.isEdit = false
+        if(!e.target.value.trim()) return alert('输入不能为空！')
+        this.$bus.$emit('updateTodo', todo.id, e.target.value)
       }
     }
   }
